dc99f77ae1feee5d02354dd0be3fa0c1670e9d4f
EC2 Default User <ec2-user@ip-172-31-17-7.eu-central-1.compute.internal>
2023-11-14 11:13:41 +0000

    state file added

---
+++ prod/state-file.tf
@@ -0,0 +1,14 @@
+resource "aws_s3_bucket" "bucket" {
+  bucket = "state-infra-pragma-kris"
+}
+
+resource "aws_dynamodb_table" "terraform_state_lock" {
+  name           = "terraform_state_lock"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "LockID"
+  
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
