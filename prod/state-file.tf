d3981e8b6e0f00fb3a85c02c1b242ab09f2df00a
EC2 Default User <ec2-user@ip-172-31-17-7.eu-central-1.compute.internal>
2024-01-07 16:27:04 +0000

    added ecr

--- prod/state-file.tf
+++ prod/state-file.tf
@@ -3,9 +3,9 @@ resource "aws_s3_bucket" "bucket" {
 }
 
 resource "aws_dynamodb_table" "terraform_state_lock" {
-  name            = "terraform_state_lock"
-  billing_mode    = "PAY_PER_REQUEST"
-  hash_key        = "LockID"
+  name         = "terraform_state_lock"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
   
   attribute {
     name = "LockID"
